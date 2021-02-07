4742be1f840db443d33f220fb308a2f0c5dd9d0e
milton <MACMILTON9@gmail.com>
2021-02-07 12:54:11 -0600

    added a trajrctory

---
+++ newSketch.js
@@ -0,0 +1,28 @@
+var num = 5; // num is a number datatype
+var str = "Hello" // string datatype
+var isTrue = false; // boolean datatype
+var empty = null // null datatype means nothing 
+var temp // datatype - undefined;
+
+//Arrays - Datastructures that can hold more than 
+
+
+var arrayExample = [10,20,30,40,50];
+
+arrayExample.push(60)
+
+console.log(arrayExample);
+
+console.log(arrayExample[0]) // return the first element in the array
+
+console.log(arrayExample.length) // number of elements in array
+
+console.log(arrayExample[arrayExample.length - 1]) // last eelement in the array
+
+var arrayExample2 = ["Milton", "Sid", "Zaid", "Yuna","Joann", "Dan"]
+
+var temp = [arrayExample, arrayExample2];
+
+console.log(temp[0][0]);
+
+console.log(temp[1][4]);
